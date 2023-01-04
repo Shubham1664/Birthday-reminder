070f63c2a201c810d6f74e587c0269299b98badf
Shubham Verma <74960930+Shubham1664@users.noreply.github.com>
2023-01-04 19:07:24 +0530

    project completed

--- src/List.js
+++ src/List.js
@@ -1,9 +1,18 @@
-import React from 'react';
+ import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+     
+     return <article key={person.id} className="person">
+       <img src={person.image}/>
+       <div>
+        <h4>{person.name}</h4>
+        <p>{person.age} years</p>
+       </div>
+     </article>
+      })}
     </>
   );
 };
